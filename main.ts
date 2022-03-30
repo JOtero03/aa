eea5df0e0cb86005bf615d80a0e9e48b6fdcb862
JOtero03 <102694275+JOtero03@users.noreply.github.com>
2022-03-30 21:22:16 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    I2C_LCD1602.ShowString("Hello", 0, 0)
+    for (let index = 0; index < 4; index++) {
+        I2C_LCD1602.shr()
+        basic.pause(1000)
+    }
+})
+I2C_LCD1602.LcdInit(39)
+I2C_LCD1602.on()
+I2C_LCD1602.BacklightOn()
+I2C_LCD1602.ShowString("Hello", 0, 0)
+basic.pause(2000)
+I2C_LCD1602.clear()
 basic.forever(function () {
 	
 })
